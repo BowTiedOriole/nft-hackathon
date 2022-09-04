aabb7514dc0d168a76e1a71fd148fab84acb3936
BowTiedOriole <bowtiedoriole@protonmail.com>
2022-09-04 15:26:42 -0400

    Added code to scripts/deploy. Created .env

---
+++ scripts/deploy.js
@@ -0,0 +1,49 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    //   const [deployer] = await ethers.getSigners();
+
+    //   console.log("Deploying contracts with the account:", deployer.address);
+
+    //   console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    //   const Token = await ethers.getContractFactory("Token");
+    //   const token = await Token.deploy();
+
+    //   console.log("Token address:", token.address);
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const OrioleToken = await ethers.getContractFactory("OrioleToken");
+    const OrioleStake = await ethers.getContractFactory("OrioleStake");
+    const OrioleNFT = await ethers.getContractFactory("OrioleNFT");
+
+    const nftName = 'OrioleNFT';
+    const nftSymbol = 'ONFT';
+    const currentTime = await time.latest();
+    const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
+    const allowListStartTime = currentTime + ONE_DAY_IN_SECONDS;
+    const publicSaleStartTime = allowListStartTime + ONE_DAY_IN_SECONDS;
+
+    const OrioleNFTContract = await OrioleNFT.deploy(nftName, nftSymbol, allowListStartTime, publicSaleStartTime);
+    const OrioleTokenContract = await OrioleToken.deploy();
+    const OrioleStakeContract = await OrioleStake.deploy(OrioleNFTContract.address, OrioleTokenContract.address);
+
+
+    // Grant OrioleStake MINTER_ROLE
+    await OrioleTokenContract.connect(deployer).grantRole(OrioleTokenContract.MINTER_ROLE(), OrioleStakeContract.address)
+
+    console.log("NFT address: ", OrioleNFTContract.address)
+    console.log("Token address: ", OrioleTokenContract.address)
+    console.log("Stake address: ", OrioleStakeContract.address)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
