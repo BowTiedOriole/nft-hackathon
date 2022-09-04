10c227f902ba27a015a54375e4e1fcbd300d3100
BowTiedOriole <bowtiedoriole@protonmail.com>
2022-09-04 16:36:13 -0400

    Removed comment block from scripts/deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,6 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    //   const [deployer] = await ethers.getSigners();
-
-    //   console.log("Deploying contracts with the account:", deployer.address);
-
-    //   console.log("Account balance:", (await deployer.getBalance()).toString());
-
-    //   const Token = await ethers.getContractFactory("Token");
-    //   const token = await Token.deploy();
-
-    //   console.log("Token address:", token.address);
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying contracts with the account:", deployer.address);
